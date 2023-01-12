15c82b5cc5b65c59e1486f6ca56d7c391d5ef5dc
Jesse <jessepirrotta@gmail.com>
2023-01-12 13:35:06 -0500

    Cookie Store Program!

---
+++ cookie_store_back_end.py
@@ -0,0 +1,58 @@
+import csv
+
+selections = ['C', 'c', "Chocolate Chip", "chocolate chip", 'O', "o", "Oatmeal Raisin", "oatmeal raisin", "Sugar", 'S',
+              's', "sugar"]
+
+
+class Cookie:
+    type = "Unbaked"
+    price = 0.0
+    ice_cream_status = False
+
+    # Constructor
+    def __init__(self):
+        print("Cookie Created")
+
+    # Type Setter
+    def type_selector(self, user_type):
+        # if the selection is valid
+        if user_type in selections:
+            self.type = user_type
+        # Standardize choices (if user selected c represent as cookie)
+        if user_type == "C" or user_type == "c" or user_type == "Chocolate Chip" or user_type == "chocolate chip":
+            self.type = "Chocolate Chip"
+            self.update_price()
+        elif user_type == "O" or user_type == "o" or user_type == "Oatmeal Raisin" or user_type == "oatmeal raisin":
+            self.type = "Oatmeal Raisin"
+            self.update_price()
+        elif user_type == "S" or user_type == "s" or user_type == "Sugar" or user_type == "sugar":
+            self.type = "Sugar"
+            self.update_price()
+        # Display the selection
+        print("Type is now: " + self.type)
+
+    def update_price(self):
+        if self.type == "Chocolate Chip":
+            self.price = 1.50
+        elif self.type == "Oatmeal Raisin":
+            self.price = 1.25
+        elif self.type == "Sugar":
+            self.price = 1.00
+        else:
+            self.price = 0.0
+
+    def add_ice_cream(self, userSelection):
+        # Updating Cookie Price based on Ice Cream
+        if userSelection == "Yes" or userSelection == "yes" or userSelection == "y" or userSelection == "Y":
+            self.ice_cream_status = True
+            self.price += 0.50
+            print("Your total is: $" + str(self.price))
+            
+    def generate_recipt(self):
+        csv.writer(open("recipt.csv", "w")).writerow(["Selections", "Price"])
+        csv.writer(open("recipt.csv", "a")).writerow([self.type, self.price])
+        if self.ice_cream_status:
+            csv.writer(open("recipt.csv", "a")).writerow(["Ice Cream", 0.50])
+
+        price_as_str = str(self.price)
+        csv.writer(open("recipt.csv", "a")).writerow(["Total"] + [price_as_str])
---
+++ cookie_store_front_end.py
@@ -0,0 +1,78 @@
+# This is a simple gui program that allows you to purchase only one cookie
+# at a time. It also allows you to add ice cream to your cookie.
+# With some updates, the program could be setup for the user to purchase multiple cookies.
+# This program was only written to support the updating of one cookie as the main focus was to
+# learn who classes and guis operate in python
+# the program will also generate an invoice to a CSV file.
+
+# Written by Jesse Pirrotta
+# Date 2023-01-12
+# Version 0.5 (one cookie)
+# Updates (future): add support for multiple cookies
+
+import PySimpleGUI as sg
+
+import cookie_store_back_end
+
+# Creating Cookie
+
+newCookie = cookie_store_back_end.Cookie()
+
+total = sg.Text("Your total is: $0.00")
+# Create the windows layout
+layout = [[sg.Text("Which type of cookie would you like:")],
+          [sg.Text("Chocolate Chip (C), Oatmeal Raisin (O), or Sugar (S)?")],
+          [sg.Button('Chocolate Chip'), sg.Button('Oatmeal Raisin'), sg.Button('Sugar')],
+          [sg.Text("Would you like ice cream on that?")],
+          [sg.Button('Yes'), sg.Button('No')],
+          [total],
+          [sg.Button('Generate Recipt')]]
+
+# Create the window
+window = sg.Window('Cookie Store', layout)
+
+# Create an event loop
+while True:
+    event, values = window.read()
+    # End program if user closes window or generates their recipt
+    # update the price
+
+    if event == sg.WIN_CLOSED or event == 'Generate Recipt':
+        newCookie.generate_recipt()
+        break
+    elif event == 'Chocolate Chip':
+        newCookie.type_selector("C")
+        # newCookie.update_price()
+        total.update("Your total is: $" + str(newCookie.price))
+    elif event == 'Oatmeal Raisin':
+        newCookie.type_selector("O")
+        newCookie.update_price()
+        total.update("Your total is: $" + str(newCookie.price))
+    elif event == 'Sugar':
+        newCookie.type_selector("S")
+        newCookie.update_price()
+        total.update("Your total is: $" + str(newCookie.price))
+    elif event == 'Yes':
+        newCookie.add_ice_cream("Yes")
+        total.update("Your total is: $" + str(newCookie.price))
+    elif event == 'No':
+        newCookie.add_ice_cream("No")
+        total.update("Your total is: $" + str(newCookie.price))
+
+# Old Non GUI Version
+# # Selecting Cookie Type
+# print("Which type of cookie would you like:")
+# userSelection = input("Chocolate Chip (C), Oatmeal Raisin (O), or Sugar (S)?\n")
+#
+# # Updating Cookie Type
+# newCookie.type_selector(userSelection)
+# # Updating Cookie Price
+# newCookie.update_price()
+#
+# # Ice Cream Query
+# print("Would you like ice cream on that?")
+# userSelection = input("Yes or No?\n")
+# newCookie.add_ice_cream(userSelection);
+#
+# # Generate a recipt as a csv file
+# newCookie.generate_recipt()
